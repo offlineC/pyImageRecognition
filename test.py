aa386ecf38bf5b1a14fac1e944a41b222a12b4a8
offlineC <rochaleanne@gmail.com>
2023-09-03 22:45:17 +0800

    updated notes

--- test.py
+++ test.py
@@ -1,17 +1,30 @@
 from python_imagesearch.imagesearch import imagesearch
+import pyautogui
 
+posX = 0
+posY = 0
 
 def quickImageSearch():
-    pos = imagesearch("./imagesamples/video.png")
+    pos = imagesearch("./imagesamples/button.png")
     if pos[0] != -1:
         # prints out the x,y coordinate positions of the image
+        posX = pos[0]
+        posY = pos[1]
         print("position : ", pos[0], pos[1])
     else:
         print("image not found")
 
 
-
-    
-if __name__ == "__main__":
+def clickPoint():
+    # try:
     quickImageSearch()
+    print(type(posX))
+    # pyautogui.FAILSAFE = False
+    # pyautogui.click(x=posX, y=posY)
+    # except:
+    #     print("err")
+    
+
+if __name__ == "__main__":
+    clickPoint()
     pass
