a1e2a2fb986ea3898730a1bca638300a9a5591f0
offlineC <rochaleanne@gmail.com>
2023-09-03 22:25:29 +0800

    removed sample images

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from python_imagesearch.imagesearch import imagesearch
+
+
 def quickImageSearch():
-    pos = imagesearch("./imagesamples/apple_158989157.jpg")
+    pos = imagesearch("./imagesamples/video.png")
     if pos[0] != -1:
+        # prints out the x,y coordinate positions of the image
         print("position : ", pos[0], pos[1])
     else:
         print("image not found")
+
+
 
     
 if __name__ == "__main__":
